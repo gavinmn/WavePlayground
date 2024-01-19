670aeff4c4bd51911fd916e263aa2f5cc0829964
Gavin Nelson <gavin@nelson.co>
2024-01-19 14:42:08 -0800

    Initial Commit

---
+++ WavePlayground/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WavePlayground
+//
+//  Created by Gavin Nelson on 1/19/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WavePlayground/WavePlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  WavePlaygroundApp.swift
+//  WavePlayground
+//
+//  Created by Gavin Nelson on 1/19/24.
+//
+
+import SwiftUI
+
+@main
+struct WavePlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
